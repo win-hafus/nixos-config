5cd305a9b3cc644920726ffdc02b7ea19ad44a56
Hafus <konstantin.pirs@gmail.com>
2025-07-06 18:31:30 +0300

    change theme (fix)

--- modules/home-manager/oh-my-zsh/zsh.nix
+++ modules/home-manager/oh-my-zsh/zsh.nix
@@ -1,18 +1,19 @@
 { pkgs, ...}:
 
-# let
-  # customOhMyZshTheme = ''
-  #   autoload -U colors && colors
-  #   PROMPT='%{''${fg_bold[white]}%}[%{''${fg[blue]}%}%t %{''${fg_bold[white]}%}] %{''${fg_bold[white]}%}[ %{''${fg[green]}%}%n%{$reset_color%}@%{''${fg[magenta]}%}%m %{''${fg[blue]}%}%~''$(git_prompt_info) %{''${fg_bold[white]}% ]%{$reset_color%}
-  #   > '
-  #   ZSH_THEME_GIT_PROMPT_PREFIX="%{''${fg_bold[yellow]}%}:("
-  #   ZSH_THEME_GIT_PROMPT_SUFFIX=")%{$reset_color%}"
-  #   ZSH_THEME_GIT_PROMPT_CLEAN=" | ok"
-  #   ZSH_THEME_GIT_PROMPT_DIRTY=" | x"
-  # '';
-# in
-{
-  # home.file.".oh-my-zsh/custom/themes/hafus.zsh-theme".text = customOhMyZshTheme;
+let
+  customOhMyZshTheme = ''
+    ZSH_THEME_GIT_PROMPT_PREFIX="[ %{$fg[magenta]%}\uE0A0 "
+    ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} ]"
+    ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}!"
+    ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
+    ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+    PROMPT='
+    [ %{$fg_bold[green]%}%~%{$reset_color%} ][ %{$fg_bold[green]%}%n%{$reset_color%}@%{$fg_bold[magenta]%}%m %{$reset_color%}]$(git_prompt_info)$(virtualenv_prompt_info)%{$reset_color%}[%{$fg_bold[blue]%}%t%{$reset_color%} ]
+    > '
+  '';
+in {
+  home.file.".oh-my-zsh/custom/themes/hafus.zsh-theme".text = customOhMyZshTheme;
 
   programs.fzf = {
     enable = true;
@@ -27,7 +28,7 @@
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" "dotenv" "fzf" "man" "node" "nodenv" "npm" "pip" "postgres" "pyenv" "qrcode" ];
-      # theme = "hafus";
+      theme = "hafus";
     };
   };
 }
