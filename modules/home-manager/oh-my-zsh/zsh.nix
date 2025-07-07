76830fb83127f4e67c7a803343862a89a72be12a
Hafus <konstantin.pirs@gmail.com>
2025-07-07 04:42:09 +0300

    add a module/home-manager for short pwd

--- modules/home-manager/oh-my-zsh/zsh.nix
+++ modules/home-manager/oh-my-zsh/zsh.nix
@@ -2,32 +2,25 @@
 
 let
   customOhMyZshTheme = ''
-    ZSH_THEME_GIT_PROMPT_PREFIX="[ %{$fg[magenta]%}\uE0A0 "
-    ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} ]"
-    ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}!"
-    ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
-    ZSH_THEME_GIT_PROMPT_CLEAN=""
-
     function custom_pwd() {
       local nixos_dir="/etc/nixos"
       local pwd="''${PWD}"
-      
-      # Define directory abbreviations
-      local -A abbrev=('home-manager' 'm')
-      
+
+      typeset -A abbrev
+      abbrev=( [home-manager]=m [default]=d )
+
       if [[ "$pwd" == "$nixos_dir" ]]; then
         echo "nix-conf"
       elif [[ "$pwd" == "$nixos_dir"/* ]]; then
         local suffix="''${pwd#$nixos_dir/}"
         local parts=("''${(@s:/:)suffix}")
-        
-        # Apply abbreviations to all path components
+
         for i in {1..''${#parts}}; do
-          if [[ -n "''${abbrev[''${parts[$i]}]" ]]; then
-            parts[$i]="''${abbrev[''${parts[$i]}]"
+          if [[ -n "''${abbrev[''${parts[$i]}]}" ]]; then
+            parts[$i]="''${abbrev[''${parts[$i]}]}"
           fi
         done
-        
+
         echo "nix-conf/''${(j:/:)parts}"
       else
         print -P '%~'
@@ -36,6 +29,11 @@ let
 
     PROMPT='[ %{$fg_bold[green]%}$(custom_pwd)%{$reset_color%} ][ %{$fg_bold[green]%}%n%{$reset_color%}@%{$fg_bold[magenta]%}%m %{$reset_color%}]$(git_prompt_info)$(virtualenv_prompt_info)%{$reset_color%}[ %{$fg_bold[blue]%}%t%{$reset_color%} ]
     > '
+    ZSH_THEME_GIT_PROMPT_PREFIX="[ %{$fg[magenta]%}\uE0A0 "
+    ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} ]"
+    ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}!"
+    ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[green]%}?"
+    ZSH_THEME_GIT_PROMPT_CLEAN=""
   '';
 in {
   home.file.".oh-my-zsh/custom/themes/hafus.zsh-theme".text = customOhMyZshTheme;
