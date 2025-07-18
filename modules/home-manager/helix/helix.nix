f9190b524b136bbf33ba71b8dd1b1e7cfc23e123
win-hafus <konstantin.pirs@gmail.com>
2025-07-18 19:25:16 +0300

    fix: enable module

--- modules/home-manager/helix/helix.nix
+++ modules/home-manager/helix/helix.nix
@@ -3,24 +3,26 @@
     source = ./themes;
     recursive = true;
   };
-
-  programs.helix.settings = {
-    theme = "macchiato";
-    editor = {
-      line-number = "relative";
-      lsp.enable = true;
-      soft-wrap.enable = true;
-      search.smart-case = true;
-      lsp.display-messages = true;
-    };
-    editor.statusline = {
-      left = ["mode" "spinner"];
-      center = ["file-name" "file-modification-indicator" "read-only-indicator"];
-      right = ["diagnostics" "position" "file-type"];
-      separator = "│";
-      mode.normal = "normal";
-      mode.insert = "insert";
-      mode.select = "select";
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "macchiato";
+      editor = {
+        line-number = "relative";
+        lsp.enable = true;
+        soft-wrap.enable = true;
+        search.smart-case = true;
+        lsp.display-messages = true;
+      };
+      editor.statusline = {
+        left = ["mode" "spinner"];
+        center = ["file-name" "file-modification-indicator" "read-only-indicator"];
+        right = ["diagnostics" "position" "file-type"];
+        separator = "│";
+        mode.normal = "normal";
+        mode.insert = "insert";
+        mode.select = "select";
+      };
     };
   };
 }
