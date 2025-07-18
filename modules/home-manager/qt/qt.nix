98a2e795c9f1ff993d1c024011e932c8c8585c67
win-hafus <konstantin.pirs@gmail.com>
2025-07-18 19:25:05 +0300

    fix: add catppuccin module

--- modules/home-manager/qt/qt.nix
+++ modules/home-manager/qt/qt.nix
@@ -1,5 +1,11 @@
 {config, ...}:
 {
+  catppuccin.kvantum = {
+    enable = true;
+    flavor = "macchiato";
+    accent = "green";
+    apply = true;
+  };
   qt = {
     enable = true;
     # platformTheme = "qtct";
