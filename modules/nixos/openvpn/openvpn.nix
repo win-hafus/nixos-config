b40a79fd1b8d992ad0e6e801058d83ab8f70b03e
win-hafus <konstantin.pirs@gmail.com>
2025-07-22 00:40:09 +0300

    fix: change config path

--- modules/nixos/openvpn/openvpn.nix
+++ modules/nixos/openvpn/openvpn.nix
@@ -2,32 +2,32 @@
   services.openvpn = {
     servers = {
       nl-1 = {
-        config = "/etc/openvpn/proton-nl-1.ovpn";
+        config = '' config /etc/openvpn/proton-nl-1.ovpn '';
         autoStart = false;
         updateResolvConf = true;
       };
       nl-2 = {
-        config = "/etc/openvpn/proton-nl-2.ovpn";
+        config = '' config /etc/openvpn/proton-nl-2.ovpn '';
         autoStart = false;
         updateResolvConf = true;
       };
       jp-1 = {
-        config = "/etc/openvpn/proton-jp-1.ovpn";
+        config = ''config /etc/openvpn/proton-jp-1.ovpn '';
         autoStart = false;
         updateResolvConf = true;
       };
       jp-2 = {
-        config = "/etc/openvpn/proton-jp-2.ovpn";
+        config = '' config /etc/openvpn/proton-jp-2.ovpn '';
         autoStart = false;
         updateResolvConf = true;
       };
       ro = {
-        config = "/etc/openvpn/proton-ro.ovpn";
+        config = '' config /etc/openvpn/proton-ro.ovpn '';
         autoStart = false;
         updateResolvConf = true;
       };
       us = {
-        config = "/etc/openvpn/proton-us.ovpn";
+        config = '' config /etc/openvpn/proton-us.ovpn '';
         autoStart = false;
         updateResolvConf = true;
       };
