ea04a220df936565c325e6c489f3bcde250b6e7f
win-hafus <konstantin.pirs@gmail.com>
2025-07-22 04:59:38 +0300

    fix: changed syntax

--- modules/home-manager/hyprlock/hyprlock.nix
+++ modules/home-manager/hyprlock/hyprlock.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       background = {
-        path = ./wallpaper.png;
+        path = "${builtins.toString ./wallpaper.png}";
         blur_passes = 3;
         blur_size = 1;
         contrast = 0.8916;
@@ -21,47 +21,63 @@
       animations = {
         enabled = false;
       };
-      # Time
-      label = {
-        text = "cmd[update:1000] echo \"$(date '+%I:%M%p')\"";
-        color = "rgb(110, 154, 65)";
-        font_size = 120;
-        font_family = "Fira Code Bold";
-        position = "0, -140";
-        halign = "center";
-        valign = "top";
-      };
+      label = [
+        # Time
+        {
+          text = "cmd[update:1000] echo \"$(date '+%I:%M%p')\"";
+          color = "rgb(110, 154, 65)";
+          font_size = 120;
+          font_family = "Fira Code Bold";
+          position = "0, -140";
+          halign = "center";
+          valign = "top";
+        }
+        
+        # Date
+        {
+          text = "cmd[update:1000] echo \"<span>$(date '+%A, %d %B')</span>\"";
+          color = "rgba(225, 225, 225, 0.75)";
+          font_size = 30;
+          font_family = "Fira Code";
+          position = "0, 200";
+          halign = "center";
+          valign = "center";
+        }
 
-      # Date
-      label = {
-        text = "cmd[update:1000] echo \"<span>$(date '+%A, %d %B')</span>\"";
-        color = "rgba(225, 225, 225, 0.75)";
-        font_size = 30;
-        font_family = "Fira Code";
-        position = "0, 200";
-        halign = "center";
-        valign = "center";
-      };
+        # Login Picture
+        {
+          text = "";
+          color = "rgba(225, 225, 225, 0.65)";
+          font_size = 120;
+          position = "0, 60";
+          halign = "center";
+          valign = "center";
+        }
 
-      # Login Picture
-      label = {
-        text = "";
-        color = "rgba(225, 225, 225, 0.65)";
-        font_size = 120;
-        position = "0, 60";
-        halign = "center";
-        valign = "center";
-      };
+        # Greeting user
+        {
+          text = "Hello, $USER";
+          color = "rgba(255, 255, 255, 0.65)";
+          font_size = 25;
+          font_family = "Fira Code Bold";
+          halign = "center";
+          valign = "center";
+        }
 
-      # Greeting user
-      label = {
-        text = "Hello, $USER";
-        color = "rgba(255, 255, 255, 0.65)";
-        font_size = 25;
-        font_family = "Fira Code Bold";
-        halign = "center";
-        valign = "center";
-      };
+        # Song Name and Artist
+        {
+          text = "cmd[update:1000] ${pkgs.writeShellScript "songdetail" ''
+            song_info=$(playerctl metadata --format '{{title}}      {{artist}}')
+            echo \"$song_info\"
+          ''}";
+          color = "rgba(235, 219, 178, .75)";
+          font_size = 16;
+          font_family = "Cascadia Code";
+          position = "0, 80";
+          halign = "center";
+          valign = "bottom";
+        }
+      ];
 
       # Input Field
       input-field = {
@@ -80,20 +96,6 @@
         position = "0, -140";
         halign = "center";
         valign = "center";
-      };
-
-      # Song Name and Artist
-      label = {
-        text = "cmd[update:1000] ${pkgs.writeShellScript "songdetail" ''
-          song_info=$(playerctl metadata --format '{{title}}      {{artist}}')
-          echo \"$song_info\"
-        ''}";
-        color = "rgba(235, 219, 178, .75)";
-        font_size = 16;
-        font_family = "Cascadia Code";
-        position = "0, 80";
-        halign = "center";
-        valign = "bottom";
       };
     };
   };
