f75637aadca134ec730e4cc9eed879e611abcf2d
win-hafus <konstantin.pirs@gmail.com>
2025-08-08 12:24:47 +0300

    remove packges from home.nix

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -21,6 +21,7 @@
     ../../modules/home-manager/vscode/vscode.nix
     ../../modules/home-manager/hyprpaper/hyprpaper.nix
     ../../modules/home-manager/wlogout/wlogout.nix
+    ../../modules/home-manager/packages/packages.nix
   ];
 
   home = {
@@ -28,60 +29,6 @@
     homeDirectory = "/home/hfv5";
     stateVersion = "25.05";
 
-    packages = with pkgs; [
-      zsh
-      oh-my-zsh
-      fzf
-
-      waybar
-      wl-clipboard
-      xwayland-satellite
-
-      alacritty
-      qbittorrent
-      vlc
-      imv
-      mpv
-      rofi-wayland
-      wlogout
-      reaper
-      reaper-sws-extension
-      discord
-      pavucontrol
-      nautilus
-      obs-studio
-      qbittorrent
-      telegram-desktop
-      vscode
-      spotify
-
-      flatpak
-
-      ranger
-      helix
-      eww
-      tree
-      curl
-      p7zip
-      playerctl
-      hyprpaper
-      hyprpicker
-      nixfmt
-
-      cmake
-      cli11
-
-      dropbox-cli
-      amneziawg-tools
-      amneziawg-go
-
-      python313
-      python313Packages.pip
-      libnotify
-      go
-
-      lm_sensors
-    ];
 
     sessionVariables = {
       EDITOR = "hx";
---
+++ modules/home-manager/packages/packages.nix
@@ -0,0 +1,56 @@
+{
+  home.packages = with pkgs; [
+    zsh
+    oh-my-zsh
+    fzf
+
+    waybar
+    wl-clipboard
+    xwayland-satellite
+
+    alacritty
+    qbittorrent
+    vlc
+    imv
+    mpv
+    rofi-wayland
+    wlogout
+    reaper
+    reaper-sws-extension
+    discord
+    pavucontrol
+    nautilus
+    obs-studio
+    qbittorrent
+    telegram-desktop
+    vscode
+    spotify
+
+    flatpak
+
+    ranger
+    helix
+    eww
+    tree
+    curl
+    p7zip
+    playerctl
+    hyprpaper
+    hyprpicker
+    nixfmt
+
+    cmake
+    cli11
+
+    dropbox-cli
+    amneziawg-tools
+    amneziawg-go
+
+    python313
+    python313Packages.pip
+    libnotify
+    go
+
+    lm_sensors
+  ];
+}
