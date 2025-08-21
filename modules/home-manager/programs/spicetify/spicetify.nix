e71ddb2b26f31c76d90869956992be0e1a83c28e
win-hafus <konstantin.pirs@gmail.com>
2025-08-21 23:33:29 +0300

    fix syntax

--- modules/home-manager/programs/spicetify/spicetify.nix
+++ modules/home-manager/programs/spicetify/spicetify.nix
@@ -2,7 +2,8 @@
 
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-in {
+in
+{
   programs.spicetify = {
     enable = true;
     theme = spicePkgs.themes.catppuccin;
