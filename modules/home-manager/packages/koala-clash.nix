83b28f95883f70acbe903b3a435871e9a5d84862
win-hafus <konstantin.pirs@gmail.com>
2026-02-06 17:37:47 +0300

    move koala-clash

--- modules/home-manager/packages/koala-clash.nix
+++ modules/home-manager/packages/koala-clash.nix
@@ -1,23 +1,19 @@
 { pkgs }:
-
 pkgs.stdenv.mkDerivation rec {
   pname = "koala-clash";
   version = "0.2.10";
-
   src = pkgs.fetchurl {
     url = "https://github.com/coolcoala/clash-verge-rev-lite/releases/download/v${version}/Koala.Clash_amd64.deb";
-    sha256 = "035s6scn1f5xg3v25r76llvlngvmxlwramw40pkfkwf9j0vp6czp";  # временный хеш
+    sha256 = "035s6scn1f5xg3v25r76llvlngvmxlwramw40pkfkwf9j0vp6czp";
   };
-
   nativeBuildInputs = with pkgs; [
     dpkg
     autoPatchelfHook
     makeWrapper
-    wrapGAppsHook
+    wrapGAppsHook3
   ];
-
   buildInputs = with pkgs; [
-    webkitgtk
+    webkitgtk_4_1  # изменено с webkitgtk
     gtk3
     cairo
     gdk-pixbuf
@@ -25,23 +21,19 @@ pkgs.stdenv.mkDerivation rec {
     dbus
     openssl
     librsvg
-    libsoup
+    libsoup_3     # обновлено до libsoup_3
     libayatana-appindicator
   ];
-
   unpackPhase = ''
     dpkg-deb -x $src .
   '';
-
   installPhase = ''
     mkdir -p $out
     cp -r usr/* $out/
-    
     if [ -d opt ]; then
       cp -r opt/* $out/
     fi
   '';
-
   meta = with pkgs.lib; {
     description = "A modern GUI client based on Tauri for Clash Meta";
     homepage = "https://github.com/coolcoala/koala-clash";
