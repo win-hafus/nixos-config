afc31ed3615125f7d1e211a3db2a500abe50e8c4
win-hafus <konstantin.pirs@gmail.com>
2026-02-14 15:38:11 +0300

    set pc as server

--- modules/nixos/services/openvpn.nix
+++ modules/nixos/services/openvpn.nix
@@ -1,36 +1,41 @@
 {
-  services.openvpn = {
-    servers = {
-      nl-1 = {
-        config = ''config /etc/openvpn/proton-nl-1.ovpn '';
-        autoStart = false;
-        updateResolvConf = true;
-      };
-      nl-2 = {
-        config = ''config /etc/openvpn/proton-nl-2.ovpn '';
-        autoStart = false;
-        updateResolvConf = true;
-      };
-      jp-1 = {
-        config = ''config /etc/openvpn/proton-jp-1.ovpn '';
-        autoStart = false;
-        updateResolvConf = true;
-      };
-      jp-2 = {
-        config = ''config /etc/openvpn/proton-jp-2.ovpn '';
-        autoStart = false;
-        updateResolvConf = true;
-      };
-      ro = {
-        config = ''config /etc/openvpn/proton-ro.ovpn '';
-        autoStart = false;
-        updateResolvConf = true;
-      };
-      us = {
-        config = ''config /etc/openvpn/proton-us.ovpn '';
-        autoStart = false;
-        updateResolvConf = true;
-      };
+  networking.nat = {
+    enable = true;
+    externalInterface = "enp6s0";
+    internalInterfaces = [ "tun0" ];
+  };
+
+  services.openvpn.servers = {
+    homeVPN = {
+      config = ''
+        dev tun
+        proto udp
+        port 1194
+        topology subnet
+        
+        server 10.8.0.0 255.255.255.0
+
+        ca /var/shared/openvpn/ca.crt
+        cert /var/shared/openvpn/server.crt
+        key /var/shared/openvpn/server.key
+        dh /var/shared/openvpn/dh.pem
+        tls-auth /var/shared/openvpn/ta.key 0
+        
+        cipher AES-256-GCM
+        auth SHA256
+        
+        push "redirect-gateway def1 bypass-dhcp"
+        
+        push "dhcp-option DNS 8.8.8.8"
+        push "dhcp-option DNS 8.8.4.4"
+        
+        keepalive 10 120
+        persist-key
+        persist-tun
+        user nobody
+        group nogroup
+        verb 3
+      '';
     };
   };
 }
