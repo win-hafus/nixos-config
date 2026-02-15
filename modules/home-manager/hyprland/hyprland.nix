98d333e9d27d3de8793d6e87a3f47335064f2a5c
win-hafus <konstantin.pirs@gmail.com>
2026-02-15 13:54:13 +0300

    hotfix: changed syntax of windowrule

--- modules/home-manager/hyprland/hyprland.nix
+++ modules/home-manager/hyprland/hyprland.nix
@@ -33,107 +33,93 @@
       env = HYPRCURSOR_SIZE,16
 
 
-      ###################
-      ### PERMISSIONS ###
-      ###################
-      # ecosystem {
-      #   enforce_permissions = 1
-      # }
-
-      # permission = /usr/(bin|local/bin)/grim, screencopy, allow
-      # permission = /usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland, screencopy, allow
-      # permission = /usr/(bin|local/bin)/hyprpm, plugin, allow
-
-
       #####################
       ### LOOK AND FEEL ###
       #####################
 
       general {
-          gaps_in = 5
-          gaps_out = 50
+        gaps_in = 5
+        gaps_out = 50
 
-          border_size = 2
-          col.active_border = rgba(b7bdf8ff)
-          col.inactive_border = rgba(1e1e2e00)
-          resize_on_border = true
-          allow_tearing = false
+        border_size = 2
+        col.active_border = rgba(b7bdf8ff)
+        col.inactive_border = rgba(1e1e2e00)
+        resize_on_border = true
+        allow_tearing = false
 
-          layout = dwindle
+        layout = dwindle
       }
 
       decoration {
-          rounding = 8
-          rounding_power = 2
+        rounding = 8
 
-          # Change transparency of focused and unfocused windows
-          active_opacity = 1.0
-          inactive_opacity = 1.0
+        # Change transparency of focused and unfocused windows
+        active_opacity = 1.0
+        inactive_opacity = 1.0
 
-          shadow {
-              enabled = true
-              range = 4
-              render_power = 3
-              scale = 0.5
-              color = rgba(00000022)
-          }
+        shadow {
+          enabled = true
+          range = 4
+          render_power = 3
+          scale = 0.5
+          color = rgba(00000022)
+        }
 
-          blur {
-              enabled = true
-              size = 3
-              passes = 3
-              new_optimizations = true
+        blur {
+          enabled = true
+          size = 3
+          passes = 3
+          new_optimizations = true
 
-              vibrancy = 0.1696
-          }
+          vibrancy = 0.1696
+        }
       }
 
       animations {
-          enabled = yes, please :)
+        enabled = yes
 
-          bezier = easeOutQuint,0.23,1,0.32,1
-          bezier = easeInOutCubic,0.65,0.05,0.36,1
-          bezier = linear,0,0,1,1
-          bezier = almostLinear,0.5,0.5,0.75,1.0
-          bezier = quick,0.15,0,0.1,1
+        bezier = easeOutQuint,0.23,1,0.32,1
+        bezier = easeInOutCubic,0.65,0.05,0.36,1
+        bezier = linear,0,0,1,1
+        bezier = almostLinear,0.5,0.5,0.75,1.0
+        bezier = quick,0.15,0,0.1,1
 
-          bezier = popup, 0.05, 0.9, 0.1, 1.05
-          bezier = fade_out, 0.1, 0.9, 0.2, 1
+        bezier = popup, 0.05, 0.9, 0.1, 1.05
+        bezier = fade_out, 0.1, 0.9, 0.2, 1
 
-          bezier = pop_in, 0.1, 1, 0.25, 1.1       
-          bezier = fade_in, 0.05, 0.95, 0.1, 1     
+        bezier = pop_in, 0.1, 1, 0.25, 1.1       
+        bezier = fade_in, 0.05, 0.95, 0.1, 1     
 
-          animation = global, 1, 10, default
-          animation = border, 1, 2, default
-          animation = windows, 1, 4.79, popup
-          animation = windowsIn, 1, 4, pop_in, popin
-          animation = windowsOut, 1, 4, pop_in, popin 87%
-          animation = fadeIn, 1, 1.73, fade_in
-          animation = fadeOut, 1, 1.46, fade_out
-          animation = fade, 1, 3.03, quick
-          animation = layers, 1, 3.81, easeOutQuint
-          animation = layersIn, 1, 4, easeOutQuint, fade
-          animation = layersOut, 1, 1.5, linear, fade
-          animation = fadeLayersIn, 1, 1.79, almostLinear
-          animation = fadeLayersOut, 1, 1.39, almostLinear
-          animation = workspaces, 1, 1.94, almostLinear, fade
-          animation = workspacesIn, 1, 1.21, linear, slide
-          animation = workspacesOut, 1, 1.94, linear, slide
-
+        animation = global, 1, 10, default
+        animation = border, 1, 2, default
+        animation = windows, 1, 4.79, popup
+        animation = windowsIn, 1, 4, pop_in, popin
+        animation = windowsOut, 1, 4, pop_in, popin 87%
+        animation = fadeIn, 1, 1.73, fade_in
+        animation = fadeOut, 1, 1.46, fade_out
+        animation = fade, 1, 3.03, quick
+        animation = layers, 1, 3.81, easeOutQuint
+        animation = layersIn, 1, 4, easeOutQuint, fade
+        animation = layersOut, 1, 1.5, linear, fade
+        animation = fadeLayersIn, 1, 1.79, almostLinear
+        animation = fadeLayersOut, 1, 1.39, almostLinear
+        animation = workspaces, 1, 1.94, almostLinear, fade
+        animation = workspacesIn, 1, 1.21, linear, slide
+        animation = workspacesOut, 1, 1.94, linear, slide
       }
 
       dwindle {
-          pseudotile = true
-          preserve_split = true
+        pseudotile = true
+        preserve_split = true
       }
 
       master {
-          new_status = master
+        new_status = master
       }
 
       misc {
-          force_default_wallpaper = 0
-          disable_hyprland_logo = true
+        force_default_wallpaper = 0
+        disable_hyprland_logo = true
       }
 
       #############
@@ -141,23 +127,20 @@
       #############
 
       input {
-          kb_layout = us,ru
-          kb_variant =
-          kb_model =
-          kb_options = grp:alt_shift_toggle
-          kb_rules =
+        kb_layout = us,ru
+        kb_variant =
+        kb_model =
+        kb_options = grp:alt_shift_toggle
+        kb_rules =
 
-          follow_mouse = 1
+        follow_mouse = 1
 
-          sensitivity = 0
+        sensitivity = 0
 
-          touchpad {
-              natural_scroll = true
-          }
+        touchpad {
+          natural_scroll = true
+        }
       }
-      # gestures {
-          # workspace_swipe = true
-      # }
 
       ###################
       ### KEYBINDINGS ###
@@ -180,7 +163,7 @@
       bind = $mainMod, G, pin
       bind = $mainMod, C, centerwindow
 
-      # tyling
+      # tiling
       bind = $mainMod, V, togglefloating,
       bind = $mainMod, B, togglesplit,
       bind = $mainMod, T, pseudo,
@@ -227,7 +210,6 @@
       bind = $mainMod SHIFT, 9, movetoworkspace, 9
       bind = $mainMod SHIFT, 0, movetoworkspace, 10
 
-
       bind = $mainMod, S, togglespecialworkspace, magic
       bind = $mainMod SHIFT, S, movetoworkspace, special:magic
 
@@ -256,23 +238,21 @@
       ##############################
       ### WINDOWS AND WORKSPACES ###
       ##############################
-      layerrulev2 = animation slide, layer:notifications
-      layerrulev2 = blur, layer:notifications
-      layerrulev2 = ignorezero, layer:notifications
+      layerrule = animation slide, match:namespace notifications
 
-      windowrulev2 = float, class:^(imv)$
-      windowrulev2 = float, class:^(mpv)$
-      windowrulev2 = float, class:^(vlc)$
+      windowrule = float on, match:class ^(imv)$
+      windowrule = float on, match:class ^(mpv)$
+      windowrule = float on, match:class ^(vlc)$
 
-      windowrulev2 = center, class:^(imv)$
-      windowrulev2 = center, class:^(mpv)$
-      windowrulev2 = center, class:^(vlc)$
+      windowrule = center on, match:class ^(imv)$
+      windowrule = center on, match:class ^(mpv)$
+      windowrule = center on, match:class ^(vlc)$
 
-      # Ignore maximize requests from apps. You'll probably like this.
-      windowrulev2 = suppressevent maximize, class:.*
+      # Ignore maximize requests from apps
+      windowrule = suppress_event maximize, match:class .*
 
       # Fix some dragging issues with XWayland
-      windowrulev2 = nofocus, class:^$, title:^$, xwayland:1, floating:1, fullscreen:0, pinned:0
+      windowrule = no_focus on, match:class ^$, match:title ^$, match:xwayland 1, match:float 1, match:fullscreen 0, match:pin 0
     '';
   };
 }
