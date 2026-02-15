42ef3798214e7596099e2f49ff70e4190db5b61b
win-hafus <konstantin.pirs@gmail.com>
2026-02-15 17:13:09 +0300

    add: create home-modules.nix and core-modules.nix for clear import modules

---
+++ modules/home-manager/home-modules.nix
@@ -0,0 +1,11 @@
+{
+  import = [
+    ../../modules/home-manager/hyprland/hyprland.nix
+    ../../modules/home-manager/packages/packages.nix
+    ../../modules/home-manager/programs/programs.nix
+    ../../modules/home-manager/services/services.nix
+    ../../modules/home-manager/reaper/reaper.nix
+    ../../modules/home-manager/themes/themes.nix
+    ../../modules/home-manager/xdg/xdg.nix
+  ];
+}
---
+++ modules/nixos/core-modules.nix
@@ -0,0 +1,10 @@
+{
+  import = [
+    ../../modules/nixos/systemd/systemd.nix
+    ../../modules/nixos/services/services.nix
+    ../../modules/nixos/network/network.nix
+    ../../modules/nixos/packages/packages.nix
+    ../../modules/nixos/grub/grub.nix
+    ../../modules/nixos/sops/sops.nix
+  ];
+}
