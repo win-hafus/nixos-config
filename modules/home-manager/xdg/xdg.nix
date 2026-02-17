ca9c26d8ef00d891dc4af6b646aba4ae9a219d49
win-hafus <konstantin.pirs@gmail.com>
2026-02-17 14:15:33 +0300

    refactor: syntax and add default browser through mime

--- modules/home-manager/xdg/xdg.nix
+++ modules/home-manager/xdg/xdg.nix
@@ -1,39 +1,51 @@
 {
-  xdg.desktopEntries = {
-    rofi = {
-      type="Application";
-      name="Rofi";
-      exec="rofi -show";
-      terminal=false;
-      noDisplay=true;
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = [ "zen-beta.desktop" ];
+        "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+        "x-scheme-handler/https" = [ "zen-beta.desktop" ];
+        "x-scheme-handler/about" = [ "zen-beta.desktop" ];
+        "x-scheme-handler/unknown" = [ "zen-beta.desktop" ];
+      };
     };
-    rofi-theme-selector = {
-      type="Application";
-      name="Rofi Theme Selector";
-      exec="rofi-theme-selector";
-      terminal=false;
-      noDisplay=true;
-    };
-    Helix = {
-      type="Application";
-      name="Helix";
-      exec="hx %F";
-      terminal=true;
-      noDisplay=true;
-    };
-    mpv = {
-      type="Application";
-      name="mpv Media Player";
-      exec="mpv --player-operation-mode=pseudo-gui -- %U";
-      terminal=false;
-      noDisplay=true;
-    };
-    htop = {
-      type="Application";
-      name="Htop";
-      exec="htop";
-      terminal=true;
-      noDisplay=true;
+    desktopEntries = {
+      rofi = {
+        type = "Application";
+        name = "Rofi";
+        exec = "rofi -show";
+        terminal = false;
+        noDisplay = true;
+      };
+      rofi-theme-selector = {
+        type = "Application";
+        name = "Rofi Theme Selector";
+        exec = "rofi-theme-selector";
+        terminal = false;
+        noDisplay = true;
+      };
+      Helix = {
+        type = "Application";
+        name = "Helix";
+        exec = "hx %F";
+        terminal = true;
+        noDisplay = true;
+      };
+      mpv = {
+        type = "Application";
+        name = "mpv Media Player";
+        exec = "mpv --player-operation-mode=pseudo-gui -- %U";
+        terminal = false;
+        noDisplay = true;
+      };
+      htop = {
+        type = "Application";
+        name = "Htop";
+        exec = "htop";
+        terminal = true;
+        noDisplay = true;
+      };
     };
   };
 }
