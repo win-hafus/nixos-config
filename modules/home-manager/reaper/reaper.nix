66b54502de6b4850a4b8919239be9aad9a6639fb
win-hafus <konstantin.pirs@gmail.com>
2026-03-19 00:45:31 +0300

    refactor: add archive

--- modules/home-manager/reaper/reaper.nix
+++ modules/home-manager/reaper/reaper.nix
@@ -1,28 +1,9 @@
+{ pkgs, lib, ... }:
+
 {
-  home = {
-    file.".config/REAPER/" = {
-      source = ./ContextMenuTheme;
-      recursive = true;
-    };
-    file.".config/REAPER/UserPlugins/" = {
-      source = ./SWSPlugin;
-      recursive = true;
-    };
-    file.".config/REAPER/ColorThemes/" = {
-      source = ./ColorThemes;
-      recursive = true;
-    };
-    file.".config/REAPER/Scripts/" = {
-      source = ./SWScripts;
-      recursive = true;
-    };
-    file.".config/REAPER/Scripts/HeDaScripts/" = {
-      source = ./HeDaScripts;
-      recursive = true;
-    };
-    file.".config/REAPER/Data/toolbar_icons/" = {
-      source = ./HeDaData/toolbar_icons;
-      recursive = true;
-    };
-  };
+  home.activation.unpackReaperConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -d "$HOME/.config/REAPER" ]; then
+      ${pkgs.gnutar}/bin/tar -xzf ${./reaper-config.tar.gz} -C "$HOME/.config/"
+    fi
+  '';
 }
